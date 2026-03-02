b082f48a3b63f5e89b9af47d1a62ed4948ecc9f3
Splayer0 <mynamesplayer@gmail.com>
2026-03-02 14:48:39 +0530

    added a state value and check function for almost every option in states.py and gui.py (major progress)

--- src/drives/states.py
+++ src/drives/states.py
@@ -1,3 +1,10 @@
-#####THIS FILE HOLDS ALL THE STATES OF THE OPTION CHOSEN BY THE USER#####
+##### THIS FILE HOLDS ALL THE STATES OF THE OPTION CHOSEN BY THE USER #####
 
-currentFS = None
+currentFS = 0
+image_option = 0
+partition_scheme = 0
+target_system = 0
+cluster_size = 0
+QF = 0
+create_extended = 0
+check_bad = 0
